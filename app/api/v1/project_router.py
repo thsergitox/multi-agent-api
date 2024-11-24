8a8cbeae33f64af57a2f54285a8ebed271c91ce2
thsergitox <sergiopezoj@gmail.com>
2024-11-24 01:16:13 -0500

    feat: add initial API router for project management with TODO for chat and document state retrieval

---
+++ app/api/v1/project_router.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+## TODO: Esto debe devolver el ultimo estado un chat de IA y de pdf y de papers también 
+## (El último estado de un documento se guarda en el otro servidor por lo que no es necesario hacerlo aquí)
